df5a3c751c6776596239ca0d4e304382e417bd06
Bulka148 <ismagilov-bulat@list.ru>
2022-04-07 17:24:16 +0300

    commit

--- kr1/kr1_v2_n34.py
+++ kr1/kr1_v2_n34.py
@@ -1,4 +1,4 @@
-#2 ВАРИАНТ (3 ЗАДАНИЕ И 4 ЗАДАНИЕ И 5 ЗАДАНИЕ)
+#2 ВАРИАНТ (3 ЗАДАНИЕ И 4 ЗАДАНИЕ)
 import re
 
 #3
@@ -15,9 +15,3 @@ stroka = '011011111 1010000111 1010100110 011101 10 1 0 11110 10100 1100011 1010
 print(re.findall(r'011\d*', stroka))
 print(re.findall(r'\d*010\d*', stroka))
 print(re.findall(r'\d*00\d*11|\d*11\d*00\d*', stroka))
-
-#5
-pats = [r'\b1(0|11)\b', r'\b[01]?10?1?\b', r'\b11(00|11|11)?\b']
-with open('task5_output', 'w') as f:
-    for p in pats:
-        f.write(str(re.findall(p, stroka)) + '\n')
