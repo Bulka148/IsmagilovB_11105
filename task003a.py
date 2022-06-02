9c502545fb199b20cd37e3eac9182d715fcd5f95
Bulka148 <ismagilov-bulat@list.ru>
2022-06-02 19:23:03 +0300

    homework_task003a_fix

--- task003a.py
+++ task003a.py
@@ -1,33 +1,15 @@
-class Element:
-    def __init__(self):
-        self.text = self.type = self.prior = None
-
-a = input().split()
-
-elms = []
-for i in range(len(a)):
-    el = Element()
-    el.text = a[i]
-    if 'a' <= a[i].lower() <= 'z':
-        el.type, el.prior = 'val', 3
-    if a[i] in ['+', '-', '*', '/']:
-        el.type, el.prior  = 'oper', 2 if a[i] in ['*', '/'] else 1
-    elms.append(el)
-
-
+st = input().split()
 i = 0
-while len(elms) != 1:
-    if elms[i].type == elms[i+1].type == 'val' and elms[i+2].type == 'oper':
-        if elms[i].prior < elms[i+2].prior:
-            elms[i].text = '(' + elms[i].text + ')'
-        if elms[i+1].prior <= elms[i+2].prior:
-            elms[i+1].text = '(' + elms[i+1].text + ')'
-        elms[i].text = elms[i].text + ' ' + elms[i+2].text + ' ' + elms[i+1].text
-        elms[i].prior = elms[i+2].prior
-        elms.pop(i + 1)
-        elms.pop(i + 1)
-        i = 0
-    else:
-        i += 1
+while i < len(st):
+    if (st[i] == '+' or
+        st[i] == '-' or
+        st[i] == '*' or
+        st[i] == '/'):
 
-print(elms[0].text)
+        st[i-2] = '('+st[i-2]+st[i]+st[i-1]+')'
+        st.pop(i) and st.pop(i-1)
+        i = i - 1
+
+    else:
+        i = i + 1
+print(st.pop())
