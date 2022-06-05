bb62f15e5abab4c9a133a66241c4405277fa834f
Bulka148 <ismagilov-bulat@list.ru>
2022-06-05 23:36:29 +0300

    homework_task012_fix

--- task012.py
+++ task012.py
@@ -1,49 +1,41 @@
 from functools import partial
 
 
-class Some_class:
-    def calc(self,
-             var1: int = 0, opr1: str = '+',
-             var2: int = 0, opr2: str = '+',
-             var3: int = 0, opr3: str = '+',
-             var4: int = 0, opr4: str = '+',
-             var5: int = 0) -> int:
-        logic = {'+': lambda a, b: a + b,
-                 '-': lambda a, b: a - b,
-                 '*': lambda a, b: a * b,
-                 '/': lambda a, b: a / b}
-        return logic[opr4](logic[opr3](logic[opr2](logic[opr1](var1, var2), var3), var4), var5)
+class My_class:
+    def my_calculator1(self, v1: int = 0, f1: str = '-', v2: int = 0, f2: str = '-',
+                       v3: int = 0, f3: str = '-', v4: int = 0, f4: str = '-', v5: int = 0) -> int:
+        my_sum = v1
+        for f, v in [(f1, v2), (f2, v3), (f3, v4), (f4, v5)]:
+            my_sum = eval(str(my_sum) + f + str(v))
+        return my_sum
 
-    def spec_calc(self, var1: int):
-        def opr_1(opr1: str):
-            def var_2(var2: int):
-                def opr_2(opr2: str):
-                    def var_3(var3: int):
-                        def opr_3(opr3: str):
-                            def var_4(var4: int):
-                                def opr_4(opr4: str):
-                                    def var_5(var5: int) -> int:
-                                        logic = {'+': lambda a, b: a + b,
-                                                 '-': lambda a, b: a - b,
-                                                 '*': lambda a, b: a * b,
-                                                 '/': lambda a, b: a / b}
-                                        return logic[opr4](logic[opr3](logic[opr2](logic[opr1](var1, var2),
-                                                                                   var3), var4), var5)
-                                    return var_5
-                                return opr_4
-                            return var_4
-                        return opr_3
-                    return var_3
-                return opr_2
-            return var_2
-        return opr_1
+    def my_calculator2(self, v1: int):
+        def function1(f1: str):
+            def variable2(v2: int):
+                def function2(f2: str):
+                    def variable3(v3: int):
+                        def function3(f3: str):
+                            def variable4(v4: int):
+                                def function4(f4: str):
+                                    def variable5(v5: int) -> int:
+                                        my_sum = v1
+                                        for f, v in [(f1, v2), (f2, v3), (f3, v4), (f4, v5)]:
+                                            my_sum = eval(str(my_sum) + f + str(v))
+                                        return my_sum
+                                    return variable5
+                                return function4
+                            return variable4
+                        return function3
+                    return variable3
+                return function2
+            return variable2
+        return function1
 
 
-sc = Some_class()
-print(sc.calc(1, '+', 2, '-', 3, '+', 4, '*', 5))
-print(sc.spec_calc(1)('*')(2)('+')(3)('-')(4)('+')(5))
+my_obj = My_class()
+my_calculator3 = partial(my_obj.my_calculator1, v1=1, v2=2, v3=3, v4=3, v5=2)
 
-calc_par = partial(sc.calc, var1=1, var2=2, var3=3, var4=4, var5=5)
-print('(((1+2)+3)*4)+5 =', calc_par(opr1='+', opr2='+', opr3='*', opr4='+'))
-print('(((1*2)+3)-4)/5 =', calc_par(opr1='*', opr2='+', opr3='-', opr4='/'))
-print('(((1-2)-3)*4)*5 =', calc_par(opr1='-', opr2='-', opr3='*', opr4='*'))
+print(my_obj.my_calculator1(1, '+', 2, '+', 3, '/', 3, '*', 2))
+print(my_obj.my_calculator2(1)('+')(2)('+')(3)('/')(3)('*')(2))
+print(my_calculator3(f1='+', f2='+', f3='/', f4='*'))
+print(my_calculator3(f1='*', f2='-', f3='-', f4='+'))
